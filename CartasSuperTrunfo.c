b6180da1675666f05c7b0c6b8f9298bd278f9800
will-car <willames2211@gmail.com>
2025-08-25 20:04:56 +0000

    criei primeiro printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
 //Teste larissa
 
 int main() {
+    printf("desafio super trunfo will\n");
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
